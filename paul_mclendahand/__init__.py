e20234c8d02ae9417601808a394c5d84e606f1f7
Will Kahn-Greene <willkg@mozilla.com>
2020-04-07 10:54:21 -0400

    Update for v1.2.0 development

--- paul_mclendahand/__init__.py
+++ paul_mclendahand/__init__.py
@@ -9,6 +9,6 @@ __author__ = "Will Kahn-Greene"
 __email__ = "willkg@mozilla.com"
 
 # yyyymmdd
-__releasedate__ = "20200407"
+__releasedate__ = ""
 # x.y.z or x.y.z.dev0
-__version__ = "1.1.0"
+__version__ = "1.2.0.dev0"
